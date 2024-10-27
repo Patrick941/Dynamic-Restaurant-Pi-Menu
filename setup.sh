2c9d64306c61793b04d206be4abd6504d6e46fb8
farmerp <farmerp@tcd.ie>
2024-10-27 22:42:20 +0000

    Forced sourcing of setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "This script must be sourced. Run 'source ./setup.sh' instead of './setup.sh'"
+    exit 1
+fi
+
 $(which python3) -m venv menuVenv
 source menuVenv/bin/activate
 pip install pillow
